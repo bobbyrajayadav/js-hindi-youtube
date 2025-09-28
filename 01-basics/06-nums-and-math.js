c2256ac9c166e14438523b42994b638ebcc12767
Bobby raja yadav <155613388+bobbyrajayadav@users.noreply.github.com>
2025-09-28 17:49:29 +0000

    math and num

---
+++ 01-basics/06-nums-and-math.js
@@ -0,0 +1,34 @@
+// const score = 400
+// console.log(score);
+
+
+// const balance = new Number(100)
+// console.log(balance);
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(4));
+
+// const othrNo = 23.9859651
+// console.log(othrNo.toPrecision(5));
+
+// const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-in'));
+
+// ************************************Maths*******************************
+
+// console.log(Math)
+// console.log(Math.round(5.68))
+// console.log(Math.floor(5.68))
+
+// console.log(Math.ceil(5.28))
+// console.log(Math.abs(-4))
+
+// console.log(Math.random())
+// console.log(Math.floor(Math.random()*10))
+
+const min = 10
+const max = 20
+
+// gives random value between 10 - 20
+console.log( Math.floor(Math.random() * (max - min + 1)) + min);
+
+
