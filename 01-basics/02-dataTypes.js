f88b30034e513914b9b89946579fc26eb03aba56
Bobby raja yadav <155613388+bobbyrajayadav@users.noreply.github.com>
2025-09-28 14:00:05 +0000

    datatypes

---
+++ 01-basics/02-dataTypes.js
@@ -0,0 +1,24 @@
+"use strict" // treat all code as newer version 
+
+// console.log(2 + 3); console.log("hello"); not good 
+
+// number => 2 to power 53
+// bigint
+// string => """
+// boolean => true/false
+// null => standalone value 
+// undefined => value not assigned 
+// symbol => unique 
+
+
+// object 
+
+console.log(typeof "bobby"); // string 
+
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined 
+
+
+
+
+
