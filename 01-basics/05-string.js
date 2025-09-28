db29a79cca544f12b6c7ef6322022f7002c6ec5e
Bobby raja yadav <155613388+bobbyrajayadav@users.noreply.github.com>
2025-09-28 17:16:35 +0000

    strings

---
+++ 01-basics/05-string.js
@@ -0,0 +1,30 @@
+const fname = "Bobby"
+const lname =" Yadav"
+
+console.log(`hello my name is ${fname + lname }`);
+
+const gameName = new String("     FreeFire")
+
+console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+
+console.log(gameName.length);
+
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('r'));
+console.log(gameName.substring(0,5));
+console.log(gameName);
+console.log(gameName.trim());
+
+const url = "https://bobby%20yadav.com"
+console.log(url.replace('%20','-'));
+console.log(url.includes('yadav'));
+
+const clgName = "sage-University-indore"
+
+console.log(clgName.split('-'));
+
+
+
