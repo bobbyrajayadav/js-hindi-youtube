753a5e1fa6f077208ed12a3c7b577e636fda5cd5
Bobby raja yadav <155613388+bobbyrajayadav@users.noreply.github.com>
2025-09-28 13:59:34 +0000

    variables

---
+++ 01-basics/01-variables.js
@@ -0,0 +1,14 @@
+const accountId = 123
+let accountEmail = "abc@abc.com"
+var accountPassword = "12345"
+accountCity = "Lalitpur"
+
+// accountId = 2  //not allowed 
+
+accountEmail = "xyz@abc.com"
+accountPassword = "56789"
+accountCity = "indore"
+
+console.table([ accountEmail, accountPassword, accountCity]);
+
+
